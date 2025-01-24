b99f8edb72cd06f59ba8e45a92ee07a51253fbae
Fanora Ssh Prem <hesaceza476@gmail.com>
2025-01-25 03:59:12 +0700

    Update setup1.sh

--- badvpn/setup1.sh
+++ badvpn/setup1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-REPO="https://raw.githubusercontent.com/Rerechan02/indo-ssh/main/"
+REPO="https://raw.githubusercontent.com/FanoraSsh/Autossh/main/"
 wget -O /usr/sbin/badvpn "${REPO}badvpn/badvpn" >/dev/null 2>&1
 chmod +x /usr/sbin/badvpn > /dev/null 2>&1
 wget -q -O /etc/systemd/system/badvpn1.service "${REPO}badvpn/badvpn1.service" >/dev/null 2>&1
