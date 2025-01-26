d3b67d853423a553f3bf33c47bf9d428a422d6cb
Fanora Ssh Prem <hesaceza476@gmail.com>
2025-01-27 04:56:04 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-repo="https://raw.githubusercontent.com/FanoraSsh/autoscriptssh/main/"
+repo="https://raw.githubusercontent.com/FanoraSsh/Autossh/main/"
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
